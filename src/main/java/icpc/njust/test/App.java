816e33ca0c746958c96cb759aca0324c8166686b
henryrabbit <673345280@qq.com>
2018-12-22 21:10:01 +0800

    更新hibernate配置

--- src/main/java/icpc/njust/test/App.java
+++ src/main/java/icpc/njust/test/App.java
@@ -1,13 +1,24 @@
 package icpc.njust.test;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public void initTable(){
+        //加载配置，初始化
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app=new App();
+        app.initTable();
+
     }
 }
