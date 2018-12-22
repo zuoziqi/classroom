12a16db52b25945d5d3142096ee0781cd1f71235
EGN <njc40212533145@163.com>
2018-12-22 17:43:29 +0800

    实体类

---
+++ src/main/java/icpc/njust/test/table/PersonalinformationEntity.java
@@ -0,0 +1,118 @@
+package icpc.njust.test.table;
+
+import javax.persistence.*;
+
+/**
+ * Created by DELL on 2018/12/22.
+ */
+@Entity
+@Table(name = "personalinformation", schema = "", catalog = "classroom")
+public class PersonalinformationEntity {
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+    private String school;
+    private String academy;
+    private String identity;
+
+    @Id
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "phone")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "school")
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    @Basic
+    @Column(name = "academy")
+    public String getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(String academy) {
+        this.academy = academy;
+    }
+
+    @Basic
+    @Column(name = "identity")
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonalinformationEntity that = (PersonalinformationEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (school != null ? !school.equals(that.school) : that.school != null) return false;
+        if (academy != null ? !academy.equals(that.academy) : that.academy != null) return false;
+        if (identity != null ? !identity.equals(that.identity) : that.identity != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (school != null ? school.hashCode() : 0);
+        result = 31 * result + (academy != null ? academy.hashCode() : 0);
+        result = 31 * result + (identity != null ? identity.hashCode() : 0);
+        return result;
+    }
+}
