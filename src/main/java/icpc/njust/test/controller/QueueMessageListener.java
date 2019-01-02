7b66412deb11b3b90129769cd53f051f0e430cb6
左子祺 <zuoziqi@163.com>
2019-01-02 14:41:44 +0800

    JMS Context Addition
    Signed-off-by: 左子祺 <zuoziqi@163.com>

---
+++ src/main/java/icpc/njust/test/controller/QueueMessageListener.java
@@ -0,0 +1,11 @@
+package icpc.njust.test.controller;
+
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+public class QueueMessageListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+
+    }
+}
